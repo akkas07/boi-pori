424e88f7a91a285426ae03cf4df6c5032555cd30
Ariyan Ahmed <ariyanahmed@example.com>
2026-05-03 11:08:20 +0600

    Footer Section

--- boi-pori/src/App.jsx
+++ boi-pori/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import FeaturedBooks from "./components/FeaturedBooks";
+import Footer from "./components/Footer";   // Footer import
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Banner />
       <FeaturedBooks />
+      <Footer />   {/* Footer যোগ করা হলো */}
     </>
   );
 }
--- boi-pori/src/components/Footer.jsx
+++ boi-pori/src/components/Footer.jsx
@@ -6,25 +6,32 @@ const Footer = () => {
       {/* Social Icons */}
       <div className="social-icons">
         <a href="#"><i className="fab fa-twitter"></i></a>
-        <a href="#"><i className="fab fa-google-plus-g"></i></a>
         <a href="#"><i className="fab fa-facebook-f"></i></a>
         <a href="#"><i className="fab fa-youtube"></i></a>
-        <a href="#"><i className="fab fa-vimeo-v"></i></a>
         <a href="#"><i className="fab fa-instagram"></i></a>
+      </div>
+
+      {/* Quick Links */}
+      <div className="footer-links">
+        <a href="#">Home</a>
+        <a href="#">Books</a>
+        <a href="#">Contact</a>
+        <a href="#">About</a>
       </div>
 
       <hr />
 
       {/* Footer Bottom */}
       <div className="footer-bottom">
-        <p>Copyright © BoiPori. All Rights Reserved.</p>
+        <p>© BoiPori. All Rights Reserved.</p>
         <div className="payment-icons">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/Visa.svg" alt="Visa" />
+          {/* International Payments */}
           <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="PayPal" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Mastercard-logo.png" alt="MasterCard" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/2checkout_logo.png" alt="2CO" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Skrill_logo.svg" alt="Moneybookers" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Cirrus_logo.svg" alt="Cirrus" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="MasterCard" />
+
+          {/* Bangladeshi Mobile Banking */}
+          <img src="https://download.logo.wine/logo/BKash/BKash-Logo.wine.png" alt="bKash" />
+          <img src="https://download.logo.wine/logo/Nagad/Nagad-Logo.wine.png" alt="Nagad" />
         </div>
       </div>
     </footer>
